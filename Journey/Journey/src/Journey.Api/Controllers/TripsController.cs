0a398356f236058815cebfcfc265aa6a86241e11
Alexandre Abreu <contatoalexandre95@gmail.com>
2024-07-11 16:52:28 -0300

    Mastering .NET fundamentals to create a solid foundation for world travel

---
+++ Journey/Journey/src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,21 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost] //criando uma viajem.
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            var useCase = new RegisterTripUseCase();
+            
+            useCase.Execute(request);
+            
+            return Created();//Função já apresentará cód. 201
+        }
+    }
+}
---
+++ Journey/Journey/src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Journey.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute (RequestRegisterTripJson request)
+        {
+        }
+
+        //Função auxiliar
+        private void Validate(RequestRegisterTripJson request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+               throw new ArgumentException("Nome não pode ser vazio.");
+
+            if (request.StartDate < DateTime.UtcNow.Date)
+               throw new ArgumentException("A viagem não pode ser registrada para uma data passada.");           
+
+            if (request.EndDate < request.StartDate.Date)
+               throw new ArgumentException("A viagem deve terminar após a data de início.");            
+        }
+        //43:41
+    }
+}
